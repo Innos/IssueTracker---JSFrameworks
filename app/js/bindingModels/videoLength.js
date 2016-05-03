924e216e8126b85622f0d69a85b03ad3617035c8
Innos <victor.d.kazakov@gmail.com>
2016-05-03 23:30:09 +0300

    completed Dashboard

--- app/js/bindingModels/videoLength.js
+++
@@ -1,9 +0,0 @@
-var VideoLength = function(){
-    function VideoLength(hours,minutes,seconds){
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
-    }
-
-    return VideoLength;
-}();
